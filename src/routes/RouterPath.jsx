9a12176d15b7b5ade2880042fd0215396cdaad6b
sadath <sadathshariff65@gmail.com>
2022-03-30 11:58:10 +0530

    Feat:"Initial Folder Setup"

---
+++ src/routes/RouterPath.jsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from "react-router-dom";
+import MockAPI from "../Mockman";
+
+export const RouterPath = () => {
+  return (
+    <Routes>
+      <Route path="/mockman" element={MockAPI} />
+    </Routes>
+  );
+};
