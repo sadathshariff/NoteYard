5a3c2d6cae809444b24d38b54cff703b42e6a855
sadath <sadathshariff65@gmail.com>
2022-04-01 15:39:30 +0530

    Feat:"AddNote Component"

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { Navbar } from "./Navbar/Navbar";
 export { Button } from "./Button/Button";
+export { AddNote } from "./AddNote/AddNote";
+export { SideNav } from "./SideNav/SideNav";
+export { NoteCard } from "./NoteCard/NoteCard";
