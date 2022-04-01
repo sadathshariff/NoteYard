e9aa26e897bcd004e72f600e7b9214e9efde31be
sadath <sadathshariff65@gmail.com>
2022-04-01 15:41:52 +0530

    exporting pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,5 @@
 export { Home } from "./Home/Home";
 export { Login, SignUp } from "./Auth";
+export { Notes } from "./Notes/Notes";
+export { Archive } from "./Archive/Archive";
+export { Deleted } from "./Deleted/Deleted";
