29bba414f102a57870e93e17a3d943bba5fb77a5
sadath <sadathshariff65@gmail.com>
2022-04-04 09:16:42 +0530

    Fix:"Add Note"

--- src/components/NoteCard/NoteCard.jsx
+++ src/components/NoteCard/NoteCard.jsx
@@ -1,17 +1,18 @@
 import { FaTrash, FaArchive, FaEdit } from "react-icons/fa";
-import axios from "axios";
+import { BsPin, BsPinFill } from "react-icons/bs";
 import "./NoteCard.css";
 import { useNavigate } from "react-router-dom";
 import { useNotes, useTheme } from "../../context";
-export const NoteCard = ({ note, deleteIcon }) => {
-  const { _id, title, label, notes } = note;
+export const NoteCard = ({ noteDetails }) => {
+  const { _id, title, label, priority, notes, isPinned } = noteDetails;
   const { theme } = useTheme();
-  const { deleteNote, updateNote, setShowForm, setNote, setIsEditing } =
-    useNotes();
   const navigate = useNavigate();
 
-  const updateNoteHandler = async (note) => {
-    setIsEditing(true);
+  const { setNote, setShowForm, deleteNote, noteDispatch, noteState } =
+    useNotes();
+
+  const updateNoteHandler = (_id, note) => {
+    setNote({ ...note, isEditing: (note.isEditing = true) });
     setShowForm(true);
     setNote(note);
     navigate("/notes");
@@ -23,19 +24,37 @@ export const NoteCard = ({ note, deleteIcon }) => {
         theme === "light" ? "light-notecard" : "dark-notecard"
       } `}
     >
-      <h3 className="headline-3">{title}</h3>
-      <h4 className="headline-4">{label}</h4>
+      <div className="note-header">
+        <h3 className="headline-3">{title}</h3>
+        {isPinned ? <BsPinFill size={25} /> : <BsPin size={25} />}
+      </div>
+      <h4 className="headline-4">Priority: {priority}</h4>
       <p
         className="small-text-2"
         dangerouslySetInnerHTML={{
           __html: notes,
         }}
       ></p>
-      <div className="card-footer">
-        <FaEdit onClick={() => updateNoteHandler(note)} />
-        <FaArchive onClick={() => archiveNotes(_id, note)} />
-        {deleteIcon && <FaTrash onClick={() => deleteNote(_id)} />}
-      </div>
+      <h4 className="headline-4 label">{label}</h4>
+
+      {noteState.trashedNotes.includes(noteDetails) ? (
+        <div className="flex-end">
+          <FaTrash
+            onClick={() =>
+              noteDispatch({
+                type: "REMOVE_FROM_TRASH",
+                payload: noteDetails,
+              })
+            }
+          />
+        </div>
+      ) : (
+        <div className="card-footer">
+          <FaEdit onClick={() => updateNoteHandler(_id, noteDetails)} />
+          <FaArchive onClick={() => archiveNotes(_id, noteDetails)} />
+          <FaTrash onClick={() => deleteNote(noteDetails, noteDispatch)} />
+        </div>
+      )}
     </div>
   );
 };
