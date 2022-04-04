61b821e28c9173879948c8f954e6a8c23df40c17
sadath <sadathshariff65@gmail.com>
2022-04-04 09:16:28 +0530

    fix:"NotesPage"

--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -2,21 +2,26 @@ import "./Notes.css";
 import { AddNote, NoteCard, SideNav } from "../../components";
 import { useNotes } from "../../context";
 export const Notes = () => {
-  const { userNotes } = useNotes();
+  const { noteState } = useNotes();
+  const { notes } = noteState;
   return (
     <>
       <div className="notePage-container">
         <SideNav />
         <div className="display-notes-container">
           <AddNote />
-          {userNotes.length === 0 ? (
+          {notes.length === 0 ? (
             <h3 className="headline-3 text-center">You don't have any Notes</h3>
           ) : (
             <>
               <h3 className="headline-3 text-center">Notes</h3>
               <div className="notes">
-                {userNotes.map((note) => (
-                  <NoteCard key={note._id} note={note} deleteIcon={true} />
+                {notes.map((note) => (
+                  <NoteCard
+                    key={note._id}
+                    noteDetails={note}
+                    deleteIcon={true}
+                  />
                 ))}
               </div>
             </>
